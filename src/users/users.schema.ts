25f390a602e235102bbaf61e48693be34f75773e
Sahana <sahanasaanu799@gmail.com>
2024-11-19 10:26:46 +0530

    Added create user endpoint with validations

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -2,9 +2,24 @@ import { Schema } from 'mongoose';
 
 export const usersSchema = new Schema(
     {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true }
+        id: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
     },
     {
         timestamps: true
